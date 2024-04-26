c6c036548149598db6c5083847376c7e218b90c6
Matthew Mitchener <mattjmitchener@gmail.com>
2024-04-25 21:05:28 -0700

    Update response

--- src/app/api/health-check/route.ts
+++ src/app/api/health-check/route.ts
@@ -1,16 +1,11 @@
 export async function GET() {
   try {
-    return new Response(
-      JSON.stringify({
-        status: "success",
-      }),
-      {
-        status: 200,
-        headers: {
-          "content-type": "application/json",
-        },
-      }
-    );
+    return new Response("success", {
+      status: 200,
+      headers: {
+        "content-type": "application/json",
+      },
+    });
   } catch (error) {
     console.error(error);
 
